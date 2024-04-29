00980a88060056ff01f8b6d846f80ec0924383cc
joe-gutman <jddg5wa@gmail.com>
2024-04-29 03:36:34 -0700

    Started on Week00 tic-tac-toe

--- week00_search/tic-tac-toe/tictactoe.py
+++ week00_search/tic-tac-toe/tictactoe.py
@@ -3,10 +3,18 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
 EMPTY = None
+
+
+# custom error for invalid move
+class InvalidMoveError(Exception): 
+    def __init__(self, message="Invalid move"):
+        self.message = message
+        super().__init__(self.message)
 
 
 def initial_state():
@@ -22,42 +30,111 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    x_count = 0
+    o_count = 0
+
+    for row in board:
+        for symbol in row:
+            if symbol == X:
+                x_count += 1
+            if symbol == O:
+                o_count += 1
+    
+    if x_count + o_count == 0 or ((x_count + o_count) % 2) == 0:
+        return X
+    else:
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    current_row = 0
+    current_column = 0
+
+    possible_actions = []
+
+    for row in board:
+        for column in row:
+            if column.lower() == EMPTY:
+                possible_actions.append((current_row, current_column))
+            current_column += 1
+        current_row += 1
+
+    return set(possible_actions)
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    if board[action[0]][action[1]] != EMPTY:
+        raise InvalidMoveError("The move is not valid because the cell is already occupied.")
+    
+    new_board = copy.deepcopy(board)
+
+    new_board[action[0]][action[1]] = player(board)
+
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
 
+    move_count = 0
+
+    for row in board:
+        for symbol in row:
+            if symbol != EMPTY:
+                move_count += 1
+    
+    if move_count < 5:
+        return None
+    else:
+        symbol = None
+
+        for column in range(len(board[0])):
+            for row in range(len(board)):
+                if all(i == row[0] for i in row) and row[0] != EMPTY:
+                    return board[row][0]
+                if symbol == None:
+                    symbol = board[row][column]
+                elif board[row][column] != symbol:
+                    break
+                
+
+        if symbol != None:
+            return symbol
+    
+    return None
+        
+        
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == None:
+        return False
+    else: 
+        return True
+    
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O: 
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
